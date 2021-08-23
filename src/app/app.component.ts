7bd1affe71bd76a4d7a46b8bf196af25a2eabcbd
jordicuevas <jordicuevas@gmail.com>
2021-08-23 17:51:02 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,28 @@ export interface PeriodicElement {
   weight: number;
   symbol: string;
 }
-
+interface Food {
+  value: string;
+  viewValue: string;
+}
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: "Best", name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: "Frequent", name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: "Spender", name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: "Uncertain", name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: "Total", name: 'Boron', weight: 10.811, symbol: 'B' },
-]
+  { position: 'Best', name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 'Frequent', name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 'Spender', name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 'Uncertain', name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 'Total', name: 'Boron', weight: 10.811, symbol: 'B' }
+];
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
-ngOnInit(): void {
-  
-}
-  
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Best' },
+    { value: 'pizza-1', viewValue: 'Frequent' }
+  ];
+  ngOnInit(): void {}
 }
